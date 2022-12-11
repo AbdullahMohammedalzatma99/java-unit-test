a4f5a9ef7bd0c435e1db3975b9d3796ef9e05011
AbdullahMohammedalzatma99 <93069670+AbdullahMohammedalzatma99@users.noreply.github.com>
2022-12-11 06:37:35 +0200

    Add files via upload

---
+++ juint/src/javaapplication1/SimpleCalculator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Abdullah Mohammed
+ */
+public class SimpleCalculator {
+    public int add(int number1, int number2){
+    
+    return number1 + number2;
+    }
+}
